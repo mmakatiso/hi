9db58331cce7685fcb7ae334fb329a235f8e5583
mmakatiso <dsekonyela@gmail.com>
2025-05-21 20:49:27 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("now come on and get something to grab on now")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("hi you need to eat well come to sue")
+})
+basic.forever(function () {
+	
+})
